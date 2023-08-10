478f7a9cf0ba0542713cf4dd6d3410df4ed1e98a
DayaneF6 <dayanef652@gmail.com>
2023-08-10 14:26:17 -0300

    first project class

---
+++ primeiroprojeto.rb
@@ -0,0 +1,13 @@
+p "Digite o seu nome"
+
+nome = gets.chomp.to_s
+
+p "Digite seu sobrenome"
+
+sobrenome = gets.chomp.to_s
+
+p "Digite sua idade"
+
+idade = gets.chomp.to_i
+
+p "Seja Bem vindo(a), seus dados foram cadastrados, confira! Nome: #{nome} #{sobrenome}, idade: #{idade} anos."
